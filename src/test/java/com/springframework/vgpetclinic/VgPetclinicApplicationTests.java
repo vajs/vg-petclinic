36c824e563380396c8f63b8c253b5ebad814fcd3
Vasyl <vajs@ukr.net>
2019-03-22 18:11:29 +0200

    Initial commit

---
+++ src/test/java/com/springframework/vgpetclinic/VgPetclinicApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springframework.vgpetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VgPetclinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
