b41ac32b4eb0fb35d96bf6a6ee587c257f03ff2d
Vasyl <vajs@ukr.net>
2019-04-01 13:37:39 +0300

    3.54 add model

---
+++ src/main/java/com/springframework/vgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.springframework.vgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/springframework/vgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.springframework.vgpetclinic.model;
+
+public class Vet extends Person {
+}
