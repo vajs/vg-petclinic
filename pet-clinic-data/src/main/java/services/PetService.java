697e2b68cb4b89a02923b461f28691bf719988ea
Vasyl <vajs@ukr.net>
2019-04-06 21:30:19 +0300

    3.57 add services

---
+++ pet-clinic-data/src/main/java/services/PetService.java
@@ -0,0 +1,13 @@
+package services;
+
+import com.springframework.petclinicdata.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+        Pet findById(Long id);
+        Pet save(Pet pet);
+        Set<Pet> finaAll();
+
+}
---
+++ pet-clinic-data/src/main/java/services/VetService.java
@@ -0,0 +1,12 @@
+package services;
+
+import com.springframework.petclinicdata.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+        Vet findById(Long id);
+        Vet save(Vet vet);
+        Set<Vet> finaAll();
+}
